035acbadd5087ea5459ce329907e01342b931ba8
Nona Asatryan <nona.asatryan@Nonas-MacBook-Pro.local>
2021-10-04 12:57:02 +0400

    Initial commit

---
+++ src/Chapter1/Example.java
@@ -0,0 +1,9 @@
+package Chapter1;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+    }
+}
---
+++ src/Chapter2/Example4.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+/*
+   Demonstrate the for loop.
+   Call this file "Example4.java".
+ */
+
+public class Example4 {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++) {     // i+1
+            System.out.println("This is i: " + i);
+        }
+    }
+}
