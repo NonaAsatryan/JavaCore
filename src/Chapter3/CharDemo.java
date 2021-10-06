d8a7ac432838f644240afaa538d3281e3d58695c
nona.asatryan <nonadev@gmail.com>
2021-10-06 12:41:11 +0400

    CharDemo

--- src/Chapter3/CharDemo.java
+++ src/Chapter3/CharDemo.java
@@ -6,7 +6,7 @@ public class CharDemo {
     public static void main(String[] args) {
         char ch1, ch2;
 
-        ch1 = 88;  // code for x
+        ch1 = 88;
         ch2 = 'Y';
 
         System.out.print("ch1 and ch2: ");
