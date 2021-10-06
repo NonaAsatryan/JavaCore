82440144d32c9490e7095ba05dd740d6b52661da
nona.asatryan <nonadev@gmail.com>
2021-10-06 12:42:16 +0400

    CharDemo2

--- src/Chapter3/CharDemo2.java
+++ src/Chapter3/CharDemo2.java
@@ -9,7 +9,7 @@ public class CharDemo2 {
         ch1 = 'X';
         System.out.println("ch1 contains " + ch1);
 
-        ch1++;   // increment ch1
+        ch1++;
         System.out.println("ch1 is now " + ch1);
     }
 }
