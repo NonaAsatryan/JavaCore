970d5082eeea8c5d4793f787d96a9ab7dc8bf4ea
nona.asatryan <nonadev@gmail.com>
2021-10-22 01:52:01 +0400

    DoWhile

---
+++ src/Chapter5/DoWhile.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        do {
+            System.out.println("thick " + n);
+            n--;
+        } while (n > 0);
+    }
+}
