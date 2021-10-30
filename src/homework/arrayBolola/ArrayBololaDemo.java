911fd16172b7be9d16293c8d3fc2a260e26d6513
nona.asatryan <nonadev@gmail.com>
2021-10-31 02:36:48 +0400

    ArrayBololaDemo

---
+++ src/homework/arrayBolola/ArrayBololaDemo.java
@@ -0,0 +1,28 @@
+package homework.arrayBolola;
+
+public class ArrayBololaDemo {
+
+    public static void main(String[] args) {
+
+        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+
+        char[] bolola1 = {'b', 'o', 'b', 'o', 'l', 'a'};
+
+        char[] text = {' ', ' ', 'h', 'e', 'l', 'l', 'o', ' ', 'j', 'a', 'v', 'a', ' ', ' ', ' '};
+
+        ArrayBolola arrayBolola = new ArrayBolola();
+
+        arrayBolola.arrayBolola(bolola);
+
+        arrayBolola.bolola(bolola);
+
+        arrayBolola.bolola1(bolola);
+
+        arrayBolola.bolola2(bolola1);
+
+        arrayBolola.array(text);
+    }
+}
+
+
+
