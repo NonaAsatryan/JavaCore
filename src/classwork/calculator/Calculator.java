bff03538dca018add44882766a322c6504517803
nona.asatryan <nonadev@gmail.com>
2021-11-01 22:29:56 +0400

    Calculator

---
+++ src/classwork/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package classwork.calculator;
+
+public class Calculator {
+
+    int plus (int a, int b) {
+        return a + b;
+    }
+
+    int minus(int a, int b) {
+        return a - b;
+    }
+
+    double divide (int a, int b) {
+        return a / b;
+    }
+
+    long multiple (int a, int b) {
+        return a * b;
+    }
+}
