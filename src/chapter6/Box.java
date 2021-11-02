6e7fc01ed30403aa7652cd3d4ff0d29943ab918b
nona.asatryan <nonadev@gmail.com>
2021-11-02 22:50:13 +0400

    Box

--- src/chapter6/Box.java
+++ src/chapter6/Box.java
@@ -1,6 +1,6 @@
 package chapter6;
 
-public class Box {
+  public class Box {
     double width;
     double height;
     double depth;
@@ -8,6 +8,19 @@ public class Box {
     void volume() {
         System.out.print("Volume is ");
         System.out.println(width * height * depth);
+    }
+
+    Box() {
+        System.out.println("Constructing Box");
+        width = 10;
+        height = 10;
+        depth = 10;
+    }
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
     }
 
     double volume1() {
