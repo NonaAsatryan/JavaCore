1bbb203d564970fcad0710613797950919f8af25
nona.asatryan <nonadev@gmail.com>
2021-11-06 23:26:49 +0400

    FigurePainter

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -34,11 +34,11 @@ public class FigurePainter {
         System.out.println();
         System.out.println();
         /*
-         *
-         * *
-         * * *
-         * * * *
-         * * * * *
+                    *
+                  * *
+                * * *
+              * * * *
+            * * * * *
          */
 
         for (int i = 0; i < 5; i++) {
@@ -56,13 +56,13 @@ public class FigurePainter {
         System.out.println();
 
         /*
-         *
-         * *
-         * * *
-         * * * *
-         * * *
-         * *
-         *
+               *
+              * *
+             * * *
+            * * * *
+             * * *
+              * *
+               *
          */
         for (int i = 0; i < 4; i++) {
             for (int j = 3; j > i; j--) {
