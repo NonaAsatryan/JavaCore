b54da5ea629353198dfc799306334f2c450f8f33
nona.asatryan <nonadev@gmail.com>
2021-11-23 19:17:24 +0400

    BookStorage

--- src/homework/author/BookStorage.java
+++ src/homework/author/BookStorage.java
@@ -20,15 +20,32 @@ public class BookStorage {
 
     public void print() {
         for (int i = 0; i < size; i++) {
-            System.out.print(books[i] + " ");
+            System.out.print(books[i]);
         }
-        System.out.println();
     }
 
     public void searchByTitle(String keyword) {
         for (int i = 0; i < size; i++) {
             if (books[i].getTitle().contains(keyword)) {
                 System.out.println(books[i]);
+            }
+        }
+    }
+
+    public void searchBooksByAuthor(String email) {
+        for (int i = 0; i < size; i++) {
+            if (books[i].getAuthor().getEmail().equals(email)) {
+                System.out.println(books[i]);
+            }
+        }
+    }
+
+    public void countBooksByAuthor(String email) {
+        for (int i = 0; i < size; i++) {
+            if (books[i].getAuthor().getEmail().equals(email)){
+                System.out.println(books[i].getCount());
+            } else {
+                System.out.println("There is no book yet");
             }
         }
     }
