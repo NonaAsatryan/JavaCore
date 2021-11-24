98eefa8cbf4cefbac2d9ff220b758a39e3f9b1d7
nona.asatryan <nonadev@gmail.com>
2021-11-24 08:41:57 +0400

    StackTest1

--- src/chapter7/StackTest1.java
+++ src/chapter7/StackTest1.java
@@ -1,30 +1,24 @@
 package chapter7;
 
-public class TestStack {
+public class StackTest1 {
 
     public static void main(String[] args) {
 
-        Stack mystack1 = new Stack();
-        Stack mystack2 = new Stack();
+        Stack1 mystack1 = new Stack1(5);
+        Stack1 mystack2 = new Stack1(8);
 
-        // push some numbers onto the stack
-
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             mystack1.push(i);
         }
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 8; i++) {
             mystack2.push(i);
         }
-
-        // pop those numbers off the stack
-
         System.out.println("Stack in mystack1:");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(mystack1.pop());
         }
         System.out.println("Stack in mystack2:");
-
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             System.out.println(mystack2.pop());
         }
     }
