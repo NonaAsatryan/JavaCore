f178cf25e5af7810eaf42559666ab22a4ef1987f
nona.asatryan <nonadev@gmail.com>
2022-01-07 19:54:46 +0400

    UserStorage

--- src/homework/education/storage/UserStorage.java
+++ src/homework/education/storage/UserStorage.java
@@ -3,37 +3,34 @@ package homework.education.storage;
 import homework.education.exception.UserNotFoundException;
 import homework.education.model.Lesson;
 import homework.education.model.User;
+import homework.education.util.FileUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserStorage {
 
-    private User[] users = new User[10];
-    private int size = 0;
+    private Map<String, User> userMap = new HashMap<>();
 
     public void add(User user) {
-        if (users.length == size) {
-            extend();
-        }
-        users[size++] = user;
-    }
-
-    private void extend() {
-        User[] temp = new User[users.length + 10];
-        System.arraycopy(users, 0, temp, 0, users.length);
-        users = temp;
+        userMap.put(user.getEmail(), user);
+        FileUtil.serializeUserMap(userMap);
     }
 
     public void print() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(users[i]);
+        for (User value : userMap.values()) {
+            System.out.println(value);
         }
     }
 
-    public User getByEmail (String email) throws UserNotFoundException {
-        for (int i = 0; i < size; i++) {
-            if (users[i].getEmail().equals(email)) {
-                return users[i];
-            }
+    public User getByEmail (String email) {
+        return userMap.get(email);
+    }
+
+    public void initData() {
+        Map<String, User> userMapFromFile = FileUtil.deserializeUserMap();
+        if (userMapFromFile != null) {
+            userMap = userMapFromFile;
         }
-        throw new UserNotFoundException("User doesn't exist. Email: " + email);
     }
 }
